38243b2d1c099988c4c469bf6af45fd7c7627df9
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-04-17 08:17:18 -0600

    change exercises field to exercise

--- backend/models/exercise.py
+++ backend/models/exercise.py
@@ -8,11 +8,11 @@ class ExerciseMuscleLink(SQLModel, table=True):
     id: int | None = Field(default= None, primary_key=True)
     exercise_id: int = Field(default=None, foreign_key="exercise.id")
     musclegroup: MuscleGroup = Field(sa_column=Column(SQLEnum(MuscleGroup)))
-    exercises: 'Exercise' = Relationship(back_populates="target_muscles")
+    exercise: 'Exercise' = Relationship(back_populates="target_muscles")
 
 class Exercise(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     uuid: UUID | None = Field(default=None, unique=True)
     name: str
     description: str
-    target_muscles: list[ExerciseMuscleLink] = Relationship(back_populates="exercises")
+    target_muscles: list[ExerciseMuscleLink] = Relationship(back_populates="exercise")
