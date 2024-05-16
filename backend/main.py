9e49bb597a00b730bb65cfae1b6c10a2887bfbcb
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-05-16 09:25:36 -0600

    test *.onrender.com

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ from routes import authorization, exercises, users, workouts, workout_exercises,
 
 app = FastAPI()
 
-origins = ["https://gym-app-mike-frontend.onrender.com", "http://localhost:3000"]
+origins = ["https://*.onrender.com", "http://localhost:3000"]
 
 app.add_middleware(
     CORSMiddleware,
